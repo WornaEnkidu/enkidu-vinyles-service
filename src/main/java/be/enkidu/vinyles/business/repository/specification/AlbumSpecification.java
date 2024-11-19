74d00b3a9c2caaf91194e3451580be20340eba2c
Portable <worna.16.08.16@gmail.com>
2024-11-19 21:21:07 +0100

    Refcatorin entity and DB

--- src/main/java/be/enkidu/vinyles/business/repository/specification/AlbumSpecification.java
+++ src/main/java/be/enkidu/vinyles/business/repository/specification/AlbumSpecification.java
@@ -1,12 +1,8 @@
 package be.enkidu.vinyles.business.repository.specification;
 
-import be.enkidu.vinyles.business.domain.Album;
-import be.enkidu.vinyles.business.domain.Album_;
+import be.enkidu.vinyles.business.domain.*;
 import be.enkidu.vinyles.business.repository.critere.AlbumCritere;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +25,11 @@ public class AlbumSpecification extends AbstractSpecification<Album> {
                 .filter(StringUtils::isNotBlank)
                 .map(String::trim)
                 .ifPresent(status -> predicates.add(this.getStatusPredicate(root, cb, status)));
+
+            Optional.ofNullable(critere.getSearch())
+                .filter(StringUtils::isNotBlank)
+                .map(String::trim)
+                .ifPresent(search -> predicates.add(this.getSearchPredicate(root, cb, search)));
         }
 
         return and(cb, predicates);
@@ -36,5 +37,18 @@ public class AlbumSpecification extends AbstractSpecification<Album> {
 
     private Predicate getStatusPredicate(Root<Album> root, CriteriaBuilder cb, String status) {
         return cb.equal(cb.lower(root.get(Album_.STATUS)), status.toLowerCase());
+    }
+
+    private Predicate getSearchPredicate(Root<Album> root, CriteriaBuilder cb, String search) {
+        Join<Album, Titre> titreJoin = root.join(Album_.TITRES, JoinType.LEFT);
+        Join<Titre, Artiste> titreArtisteJoin = titreJoin.join(Titre_.ARTISTES, JoinType.LEFT);
+        Join<Album, Artiste> albumArtisteJoin = root.join(Album_.ARTISTES, JoinType.LEFT);
+
+        return cb.or(
+            likeIgnoreCase(cb, root.get(Album_.NOM), search),
+            likeIgnoreCase(cb, titreJoin.get(Titre_.NOM), search),
+            likeIgnoreCase(cb, titreArtisteJoin.get(Artiste_.NOM_ARTISTE), search),
+            likeIgnoreCase(cb, albumArtisteJoin.get(Artiste_.NOM_ARTISTE), search)
+        );
     }
 }
